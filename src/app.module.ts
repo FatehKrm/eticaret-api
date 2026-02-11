dd243518b4fcda721e1f318c9d45b6f7d28685da
FatehKrm <fatehkarampour@outlook.com>
2026-02-12 01:34:10 +0300

    .env dosyası güncellemesi

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { OrderModule } from './order/order.module';
       host : process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT || '5432'),
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      password: String(process.env.DB_PASSWORD),
       database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       synchronize: process.env.NODE_ENV !== 'production', 
